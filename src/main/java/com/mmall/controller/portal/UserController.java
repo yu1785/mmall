3b6eb8314f0b32d8159f1b98eb93066a10c6ded4
yu1785 <861046209@qq.com>
2020-06-08 13:30:46 +0800

    commit 2020.6.8

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,6 @@
+package com.mmall.controller.portal;
+
+
+public class UserController {
+
+}
---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,5 @@
+package com.mmall.service;
+
+public interface IUserService {
+    public Object login(String username,String password);
+}
